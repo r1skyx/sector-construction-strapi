ce51a734f5ba884399c62ce18c819526e07dbe0a
r.shashivari <rizah.shashivari@exali.de>
2023-05-31 13:52:26 +0200

    railway edits

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "localhost"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "admin"),
-      ssl: env.bool("DATABASE_SSL", false),
+      host: env("PGHOST", "127.0.0.1"),
+      port: env.int("PGPORT", 5931),
+      database: env("PGDATABASE", "railway"),
+      user: env("PGUSER", "postgres"),
+      password: env("PGPASSWORD", "password"),
+      ssl: env.bool(true),
     },
   },
 });
